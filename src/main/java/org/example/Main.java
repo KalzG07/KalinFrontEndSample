195c75566a371a0eb005334f665a8df9d150343d
Kalin <kalin.govender@xneelo.com>
2025-01-18 20:56:07 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+/**
+ * <h1>Header </h1>
+ * description
+ * <p>
+ *
+ * @author Kalin Govender
+ * @version 1.1
+ * @since 2025/01/18
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
